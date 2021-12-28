d7cd7b3a4cbcb6dc3480ee42fcafff422da7916b
tuvtuv1876 <87674025+tuvtuv1876@users.noreply.github.com>
2021-12-28 14:24:02 +0100

    setup react weather app on GitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Weather App</h1>
       </header>
+      <footer>
+        <a href="#" alt="">
+          open sourced
+        </a>{" "}
+        coded by tu vy ho hosted on{" "}
+        <a href="#" alt="">
+          netlify
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
