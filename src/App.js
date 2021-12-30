93be33218655e92b5d43c6a9cf67d67f003e6073
tuvtuv1876 <87674025+tuvtuv1876@users.noreply.github.com>
2021-12-30 13:12:35 +0100

    added font and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Weather from "./Weather";
+
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container app-frame">
+    <div className="App ">
+      <div className="container">
         <Weather />
         <footer>
           <a href="https://github.com/tuvtuv1876/react-weather-app" alt="">
--- src/Weather.js
+++ src/Weather.js
@@ -1,38 +1,49 @@
 import React from "react";
 
+import "./Weather.css";
+
 export default function Weather() {
   return (
     <div className="Weather">
-      <form>
-        <div className="container">
-          <div className="row">
-            <div className="col-8">
-              <input type="search" placeholder="Enter a city here" />
-            </div>
-            <div className="col-4">
-              <input className="btn btn-primary" type="submit" value="Search" />
-            </div>
+      <form className="w-100">
+        <div className="row flex">
+          <div className="col-8">
+            <input
+              type="text"
+              className="form-control"
+              id="input-text"
+              placeholder="Enter a city here"
+            />
+          </div>
+
+          <div className="col-4">
+            <input
+              type="submit"
+              id="submit-button"
+              className="btn btn-primary mb-3"
+              value="Search"
+            />
           </div>
         </div>
       </form>
-      <div className="row">
-        <div className="col-4">
+      <div className="hero row">
+        <div className="overview1 col-4 p-0">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
             alt=""
           />
-          <span className="main-temperature">21</span>°C
+          <span className="main-temperature">21</span>°C|°F
         </div>
-        <div className="col-4">
+        <div className="overview2 col-4">
           <ul>
             <li>Percipitation: 94%</li>
             <li>Humidity: 66%</li>
             <li>Wind: 31km/h</li>
           </ul>
         </div>
-        <div className="col-4">
+        <div className="overview3 col-4">
           <ul>
-            <li>Winterthur</li>
+            <li className="city">Winterthur</li>
             <li>Tuesday, 16:47</li>
             <li>Light Rain</li>
           </ul>
