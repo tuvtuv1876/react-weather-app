55d9d3b59a670d5187fb109abca6f398b0e3e5bc
tuvtuv1876 <87674025+tuvtuv1876@users.noreply.github.com>
2022-01-03 22:09:51 +0100

    added search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherHero from "./WeatherHero";
-
 import "./Weather.css";
 
 export default function Weather(props) {
   const [weather, setWeather] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     setWeather({
@@ -16,14 +17,29 @@ export default function Weather(props) {
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      loaded: true,
     });
+    setLoaded(true);
   }
 
-  if (Weather.loaded) {
+  function search() {
+    let apiKey = "b90f000267947bf1f6e5a78a0ca5e027";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+  function updateChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (loaded) {
     return (
       <div className="Weather">
-        <form className="w-100">
+        <form className="w-100" onSubmit={handleSubmit}>
           <div className="row flex">
             <div className="col-8">
               <input
@@ -40,6 +56,7 @@ export default function Weather(props) {
                 id="submit-button"
                 className="btn btn-primary mb-3"
                 value="Search"
+                onChange={updateChange}
               />
             </div>
           </div>
@@ -48,12 +65,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    let apiKey = "b90f000267947bf1f6e5a78a0ca5e027";
-    let city = "Winterthur";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-    console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse);
-
-    return "Loading..";
+    search();
+    return "Loading...";
   }
 }
